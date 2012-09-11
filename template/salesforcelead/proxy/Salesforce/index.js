b681da97555bc595280d3be25255463c245a8ec4
Mark Wallace <mark@emotive.com>
2012-09-11 14:01:04 -0700

    Update templates for usgs and saleforcelead to depend on packages from github

--- template/salesforcelead/proxy/Salesforce/index.js
+++ template/salesforcelead/proxy/Salesforce/index.js
@@ -1,1 +1,8 @@
-require('sfproxy').start();
+var emproxy = require('emproxy');
+var sforce = require('emsfdc');
+
+emproxy.init(function afterInitCallback(initialConfig) {
+    sforce.setInitialConfig(initialConfig);
+    emproxy.start(sforce.processDirective);
+});
+
