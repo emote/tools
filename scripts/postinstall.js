ab318dd422b02c75a7dbfd40e0eea2442ff503e7
mike <mike@emotive.com>
2012-10-04 13:34:17 -0700

    Add command completion for emote

---
+++ scripts/postinstall.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var fs = require('fs');
+var path = require('path');
+
+var dir = __dirname;
+if (dir.indexOf('\\') >= 0) {
+    var pwdarr = dir.split('\\');
+    pwdarr.forEach(function(row, index) {
+        if (row.indexOf(' ') >= 0) {
+            pwdarr[index] = row.split(" ").join("").substring(0,6) + "~1";
+        }
+    })
+    dir = pwdarr.join('/');
+}
+
+var exportCmd =  "export EMOTE_HOME=" + dir + "\n";
+var completionScript = fs.readFileSync(__dirname + path.sep + "emote-completion.template").toString().replace(/[\r]/g, '');
+
+fs.writeFileSync(__dirname + path.sep + "emote-completion", exportCmd + completionScript);
+console.log();
+console.log("------------------------------------------------------------------------------");
+console.log("To enable bash command completion for emote, add the line:");
+console.log();
+console.log("    . " + dir + "/emote-completion");
+console.log();
+console.log("to your .profile");
+console.log("------------------------------------------------------------------------------");
+console.log();
