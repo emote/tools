a2f7727ed078c7796f9f537db7f25ba11dbd130f
Mark Wallace <mark@emotive.com>
2012-11-30 09:07:32 -0800

    Fix for bug "SFDC Proxy adapter not reading the endpoint address properly from configuration"

--- template/_sftest/proxy/sfdcoauth/index.js
+++ template/_sftest/proxy/sfdcoauth/index.js
@@ -2,6 +2,7 @@ var emproxy = require('emproxy');
 var sforce = require('emsfdc');
 
 emproxy.init(function afterInitCallback(initialConfig) {
+    sforce.setInitialConfig(initialConfig);
     emproxy.start(sforce.processDirective);
 });
 
