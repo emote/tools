2c6240747a888642b8b65cef3738ebca6aeb5c7f
Mark Wallace <mark@emotive.com>
2012-12-17 12:39:26 -0800

    Got regression test working again after merge from master

--- test/run.sh
+++ test/run.sh
@@ -1,14 +1,14 @@
 echo "starting test"
 emote create myproj
 cd myproj
-emote add usgs
+emote add all usgs --template usgs
 emote build
 emote deploy --profile ../profile.json
 emote test --profile ../profile.json
 cd ..
 emote create sfproj
 cd sfproj
-emote add _sftest
+emote add all sfdc --template Salesforce
 emote build
 emote deploy --profile ../profile.json
 emote test --profile ../profile.json
