4deeb52a15273847860508099c904a8014e543c4
mike <mike@emotive.com>
2013-07-15 17:06:45 -0700

    Changes for global salesforce proxy

--- template/Salesforce/proxy/default/README.md
+++
@@ -1,1 +0,0 @@
-Integration proxy for Salesforce
--- template/Salesforce/proxy/default/index.js
+++
@@ -1,8 +0,0 @@
-var emproxy = require('emproxy');
-var sforce = require('emsfdc');
-
-emproxy.init(function afterInitCallback(initialConfig) {
-    sforce.setInitialConfig(initialConfig);
-    emproxy.start(sforce.processDirective);
-});
-
